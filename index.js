fa504f77a3cd45f2e58f22a8568f8bfe0c06509e
zhangjie <137548913@qq.com>
2020-04-22 23:11:50 +0800

    node

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express()
 const port = 3000
-
 const userRouter = require('./routes/userRooter');
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './static')));
 
 app.use('/', (req, res, next) => {
     console.log('中间件');
