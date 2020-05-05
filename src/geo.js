9a070241f2c30e3ddebc2ceee2e9c3472ffabe9e
zhangjie <137548913@qq.com>
2020-05-05 22:50:35 +0800

    geo

--- src/geo.js
+++ src/geo.js
@@ -3,7 +3,7 @@
 // console.log(datajson);
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
-console.log(topojson);
+import d3tip from 'd3-tip';
 
 const svg = d3.select('#mainsvg');
 const width = +svg.attr('width');
@@ -19,9 +19,23 @@ const innerHeigth = height - margin.top - margin.bottom;
 const g = svg.append('g')
   .attr('id', 'maingroup')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
+/**
+ * 
+ * const tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(d => `<strong>Country: </strong>
+  <span class='details'>${d.properties.name}<br></span>
+  <strong>Population: </strong>
+  <span class='details'>${format(d.population)}</span>`);
+ */
 const projection = d3.geoNaturalEarth1();
 const geo = d3.geoPath().projection(projection);
+
+const tip = d3tip()
+  .attr('class', 'd3-tip').html(d => d.properties.name);
+
+svg.call(tip);
 
 d3.json('./assets/geoa.json').then(data => {
   console.log(data.objects.countries)
@@ -33,4 +47,20 @@ d3.json('./assets/geoa.json').then(data => {
     .attr('d', geo)
     .attr('stroke', 'black')
     .attr('stroke-width', 1)
+    .on('mouseover', function (d) {
+      console.log(this);
+      d3.select(this)
+        .attr('opacity', 0.5)
+        .attr('stroke', 'white')
+        .attr('stroke-width', 4)
+    })
+    .on('mouseout', function (d) {
+      d3.select(this)
+        .attr('opacity', 1)
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1)
+    })
+    .on('click', (data, index, element) => {
+      tip.show(data, element[index])
+    })
 })
